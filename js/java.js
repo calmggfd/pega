85b7214532abbc96db4d6f3b43fa9cce490853f2
calmggfd <calmggfd@hotmail.com>
2023-06-06 17:30:59 -0300

    06-0623

---
+++ js/java.js
@@ -0,0 +1,45 @@
+var words = ['provides body insights.', 'builds shopper confidence.', 'provides body insights.', 'increase conversions.', 'increases repeat shoppers.', 'increase conversions.'];
+var part;
+var i = 0;
+var offset = 0;
+var len = words.length;
+var forwards = true;
+var skip_count = 0;
+var skip_delay = 15;
+var speed = 70;
+
+var wordflick = function() {
+  setInterval(function() {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    document.querySelector('.word').textContent = part;
+  }, speed);
+};
+
+window.addEventListener('DOMContentLoaded', function() {
+  wordflick();
+});
